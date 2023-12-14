8e2bf9e127a42828645516c33bba690325623258
Gaurang Telang <gstelang@gmail.com>
2023-12-14 01:40:24 -0800

    feat: partial work (pending 4, 5)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"github.com/gstelang/alert-system/alerts"
 	"time"
+	// "reflect"
 )
 
 func printJson(alert *alerts.Alert) {
@@ -19,40 +20,147 @@ func printJson(alert *alerts.Alert) {
 
 
 // Build an alert execution engine that:
-// - 1. Queries for alerts using the query_alerts API and execute them at the specified interval
-// - 2. Alerts will not change over time, so only need to be loaded once at start
-// - 3. The basic alert engine will send notifications whenever it sees a value that exceeds the critical threshold.
-// - 4. Add support for repeat intervals, so that if an alert is continuously firing it will only re-notify after the repeat interval.
-// - 5. Incorporate using the warn threshold in the alerting engine - now an alert can go between states PASS <-> WARN <-> CRITICAL <-> PASS.
+// - 1. Queries for alerts using the query_alerts API and execute them at the specified interval // Done
+// - 2. Alerts will not change over time, so only need to be loaded once at start // done
+// - 3. The basic alert engine will send notifications whenever it sees a value that exceeds the critical threshold. (Done)
+// - 4. Add support for repeat intervals, so that if an alert is continuously firing it will only re-notify after the repeat interval. (In progress)
+// - 5. Incorporate using the warn threshold in the alerting engine - now an alert can go between states PASS <-> WARN <-> CRITICAL <-> PASS. (Partially done)
 
-// const (
-// 	StatePass = "PASS"
-// 	StateWarn = "WARN"
-// 	StateCritical = "CRITICAL"
-// )
+const (
+	PassThreshold = 50
+	WarnThreshold = 100
+	CriticalThreshold = 200
+)
+
+type AlertState string
+const (
+	StatePass AlertState = "PASS"
+	StateWarn AlertState = "WARN"
+	StateCritical AlertState = "CRITICAL"
+)
+
+const MaxThresholdVal = 200
+
+type alertInfo struct {
+	times int
+	lastNotifyTime time.Time
+	repeatIntervalSec int
+	state AlertState
+}
 
 // const RepeatIntervals = 10
 
 // TODO: make it singleton
 // note: client := alerts.NewClient("") does not work outside.
-var client = alerts.NewClient("")
+var client alerts.Client
+func getClientInstance() alerts.Client {
+	if client == nil {
+		client = alerts.NewClient("")
+	}
+	return client
+}
+
+func passBeteenStates(passCh <-chan string, warnCh <-chan string, criticalCh <-chan string) {
+	for {
+		select {
+		case val := <- passCh:
+			fmt.Println("pass channel", val)
+		case val := <- warnCh:
+			fmt.Println("warn channel", val)
+		case val := <- criticalCh:
+			fmt.Println("critical channel", val)
+		default:
+			// fmt.Println("do nothing")
+		}
+	}
+}
+
+func getAlertState(val int) AlertState {
+	if val >= CriticalThreshold {
+		return StateCritical
+	} else if val >= WarnThreshold {
+		return StateWarn
+	} else if val >= PassThreshold {
+		return StatePass
+	} else {
+		// TODO: temporary to satisfy go compiler 
+		// update as per business logic
+		return StatePass
+	}
+}
+
+func processAlerts(alerts []*alerts.Alert) {
+
+	passCh := make(chan string)
+	warnCh := make(chan string)
+	criticalCh := make(chan string)
+	// alertMap := make(map[string]alertInfo)
+
+	go passBeteenStates(passCh, warnCh, criticalCh)
+
+	for _, alert := range alerts {
+		// printJson(alert)
+		// fmt.Printf("warnVal = %T\n", warnVal)
+		// fmt.Printf("criticalVal = %T\n", criticalVal)
+		criticalVal := alert.Critical.Value
+		warnVal := alert.Warn.Value
+		alertName := alert.Name
+
+		if criticalVal == MaxThresholdVal { // TODO: adjust  
+			name := alert.Name
+			msg := "Critical threshold exceeded"
+
+			// Add to alertMap
+
+			// val, ok := alertMap[alertName]
+
+			// if ok {
+			// 	alertInfo := alertMap[alertName]
+			// 	alertInfo.times = alertInfo.times + 1
+			// 	alertInfo.state = getAlertState(warnVal)
+			// } else {
+			// 	alertMap[alertName] = alertInfo{
+			// 		times = 1
+			// 		repeatIntervalSec = alert.RepeatIntervalSecs
+			// 		state = getAlertState(warnVal)
+			// 	}
+			// }
+			// alertInfo[alertName].lastNotifyTime = time.Now()
+
+			notify(name, msg)
+		}
+
+		if warnVal >= CriticalThreshold {
+			fmt.Println("write to critical channel")
+			criticalCh <- alertName
+		} else if warnVal >= WarnThreshold {
+			fmt.Println("write to warn channel")
+			warnCh <- alertName
+		} else if warnVal >= PassThreshold {
+			fmt.Println("write to pass channel")
+			passCh <- alertName
+		} else {
+			// resolve
+		}
+
+	}
+}
 
 func queryAlerts(ctx context.Context) {
-	alerts, err := client.QueryAlerts(context.Background())
+	alertClient := getClientInstance()
+	alerts, err := alertClient.QueryAlerts(context.Background())
 	if err != nil {
 		fmt.Printf("error querying alerts: %+v\n", err)
 	} else {
 		fmt.Println("here - QueryAlerts")
-		for _, alert := range alerts {
-			printJson(alert)
-			// fmt.Printf("%v\n", alert)	
-		}
+		processAlerts(alerts)
 	}
 }
 
 // Queries a particular one.
 func queryByName(name string) {
-	value, err := client.Query(context.Background(), name)
+	alertClient := getClientInstance()
+	value, err := alertClient.Query(context.Background(), name)
 	if err != nil {
 		fmt.Printf("error resolving: %v\n", err)
 	} else {
@@ -62,15 +170,20 @@ func queryByName(name string) {
 }
 
 func notify(name string, msg string) {
-	err := client.Notify(context.Background(), name, msg)
+	alertClient := getClientInstance()
+
+	err := alertClient.Notify(context.Background(), name, msg)
 	if err != nil {
 		fmt.Println("notify - ", name)
 		fmt.Printf("error notifying: %v\n", err)
+	} else {
+		fmt.Println("notified - ", name)
 	}
 }
 
 func resolve(name string) {
-	err := client.Resolve(context.Background(), name)
+	alertClient := getClientInstance()
+	err := alertClient.Resolve(context.Background(), name)
 	if err != nil {
 		fmt.Println("resolve - ", name)
 		fmt.Printf("error resolving: %v\n", err)
